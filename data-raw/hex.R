e668b55a5ae263fca54551d809be6838c7edcb53
Matthew Leonawicz <matt_leonawicz@esource.com>
2019-03-17 15:14:11 -0600

    Update logo.

--- data-raw/hex.R
+++ data-raw/hex.R
@@ -2,7 +2,7 @@
 library(hexSticker)
 library(ggplot2)
 pkg <- basename(getwd())
-user <- "username"
+user <- "leonawicz"
 account <- "github"
 
 url <- paste0(user, ".", account, ".io/", pkg)
@@ -11,9 +11,9 @@ dir.create("man/figures", showWarnings = FALSE)
 
 hex_plot <- function(out, mult = 1){
   g <- ggplot() + theme_void() + theme_transparent()
-  sticker(g, package = pkg, p_y = 1, p_color = "gray20", p_size = 20,
-          h_color = "gray20", h_fill = "burlywood1", h_size =  1.4,
-          url = url, u_color = "gray20", u_size = 3, filename = out)
+  sticker(g, package = pkg, p_y = 1, p_color = "black", p_size = 20,
+          h_color = "black", h_fill = "#FAF15B", h_size =  1.4,
+          url = url, u_color = "black", u_size = 3, filename = out)
 }
 
 hex_plot(out)
